8709f5e8a1248f1762e5c4cfc15ce322602d6f44
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-05-18 17:49:44 -0700

    working on browser search

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 // if we use forms this is needed
-// app.use(express.urlencoded( {extended: true}));
+app.use(express.urlencoded( {extended: true}));
 
 app.get('/hello', (req, res) =>{
   res.render('pages/index.ejs');
@@ -23,6 +23,20 @@ app.get('/searches/new', (req, res) =>{
   res.render('pages/searches/new.ejs');
 });
 
+app.post('/searches', (req, res) =>{
+  console.log(req.body);
+  res.send('hello there');
+});
 
+// function getBooks (req, res) {
+  
+// }
+
+function Book(obj) {
+  this.image = obj.imageLinks.small || 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = obj.volumeInfo.title || 'no title';
+  this.author = obj.volumeInfo.authors || 'no author';
+  this.description = obj.volumeInfo.description || 'no description';
+}
 // start the app
 app.listen(PORT, () => console.log(`App is up on PORT:  ${PORT}`));
